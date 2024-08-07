059223894d97efd65eb597c6f26afc85a31f1a04
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2024-08-07 16:43:55 +0100

    add book array and constructor

--- lib.js
+++ lib.js
@@ -1,9 +1,26 @@
-const myLibrary = [];
+const newBook = querySelector(".add");
 
-function Book() {
+
+const myLibrary = [ new Book(" The Hobbit", "J.R.R. Tolkien", "Fantasy",295)];
+
+function Book(title, author, genre, pages) {
   // the constructor...
+  this.title= title;
+  this.author = author;
+  this.genre = genre;
+  this.pages = pages;
+  this.read = function(){
+    let read = false;
+    if (read === true){
+      console.log(" the book is read")
+    } else{
+      console.log("book unread")
+    }
+  };
 }
 
 function addBookToLibrary() {
   // do stuff here
+  myLibrary.push = Book();
+
 }
