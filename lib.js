3869a592bf32b9dac507378389aa0738fdcaa300
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2024-08-21 11:17:20 +0100

    Add ; function arrangeBooks by genre"

--- lib.js
+++ lib.js
@@ -39,6 +39,7 @@ const closeBtn = document .querySelector(".close");
 //button that brings up a form
 addNew.addEventListener("click",()=>{
   dialog.showModal()
+  arrangeBooks();
 });
 
 //use event.preventDefault to stop the form from submitting to server
@@ -112,9 +113,6 @@ function Book(title, author, genre, pages) {
 //function to take user input and store in an array
 function addBookToLibrary(){
   
-
-  
-
   addBook.addEventListener("click",()=>{
     console.log('you clicked me');
   let book = new Book(titleInput,authorInput,genreInput,pagesInput);
@@ -122,7 +120,7 @@ function addBookToLibrary(){
   myLibrary === myLibrary.push(book);
   console.log(myLibrary)
  })
-   
+  arrangeBooks(); 
 }addBookToLibrary();
 console.log("library work please")
 
@@ -137,7 +135,7 @@ function addBookToShelf(){
   let deleteBtn = document.createElement("button");
   card.appendChild(deleteBtn);
   function toDeleteBook (){
-   card = book.delete
+   card = Book().delete
   }
 }
 } addBookToShelf();
@@ -145,20 +143,21 @@ function addBookToShelf(){
 
 function arrangeBooks(){
   if(Book.genre === "textbooks"){
-
+    const textbooks= [].push(Book());
   } else if( Book.genre === "drama"){
-
+    const drama = [].push(Book())
   } else if( Book.genre === "fiction"){
-    
+    const fiction = [].push(Book())
   }else if( Book.genre === "fantasy"){
-    
+    const fantasy = [].push(Book())
   }else if( Book.genre === "history"){
-    
+    const history = [].push(Book())
   }else if( Book.genre === "sci-fi"){
-    
+    const sciFi = [].push(Book())
   }else if( Book.genre === "poetry"){
-    
+    const poetry = [].push(Book())
   }
+ return { textbooks, drama, fiction, fantasy, history, sciFi, poetry}
 }
 
 
