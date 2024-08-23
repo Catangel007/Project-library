f91d70106db69ed817271401eb8606befc7899d5
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2024-08-23 15:37:37 +0100

    Update dialog form

--- lib.js
+++ lib.js
@@ -1,101 +1,79 @@
-  function addBookToLibrary(){
-
- 
   
-
-  
-
-}
 
 function Book(title,author, pages, read){
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.info = function (){
+    return ` ${this.name} by ${this.author}, ${this.pages}pages.`
+  }
 }
 
-function Fantasy(title, author, pages, read, genre){
-  Book.call(this, title, author, pages, read);
-
-  this.genre = genre;
-}
-
-function Fiction(title, author, pages, read, genre){
-  Book.call(this, title, author, pages, read);
-
-  this.genre = genre;
-}
-
-function Drama(title, author, pages, read, genre){
-  Book.call(this, title, author, pages, read);
-
-  this.genre = genre;
-}
-
-function Age(title, author, pages, read, genre){
-  Book.call(this, title, author, pages, read);
-
-  this.genre = genre;
-}
-
-function Poetry(title, author, pages, read, genre){
-  Book.call(this, title, author, pages, read);
-
-  this.genre = genre;
-}
-
-function SciFi(title, author, pages, read, genre){
-  Book.call(this, title, author, pages, read);
-
-  this.genre = genre;
-}
-
-function TextBooks(title, author, pages, read, genre){
-  Book.call(this, title, author, pages, read);
-
-  this.genre = genre;
-}
-
-Object.setPrototypeOf(TextBooks.prototype, Book.prototype);
-Object.setPrototypeOf(SciFi.prototype, Book.prototype);
-Object.setPrototypeOf(Poetry.prototype, Book.prototype);
-Object.setPrototypeOf(Drama.prototype, Book.prototype);
-Object.setPrototypeOf(Age.prototype, Book.prototype);
-Object.setPrototypeOf(Fantasy.prototype, Book.prototype);
-Object.setPrototypeOf(Fiction.prototype, Book.prototype);
 
 
-TextBooks.genre= "text-books";
-SciFi.genre = "sci-fi";
-Poetry.genre = "poetry";
-Drama.genre = "drama";
-Age.genre = "history";
-Fantasy.genre = "fantasy";
-Fiction.genre = "fiction";
- let subway= 9;
- let sunday= 7;
-const myLibrary = [ subway, sunday];
 
-// function addBookToLibrary(){
+
+
+
+const myLibrary = [];
+
+ function addBookToLibrary(){
   
-//   const book = new TextBooks(title.value,author.value,pages.value,read.value) ||
-//              new SciFi(title.value,author.value,pages.value,read.value) ||
-//              new Poetry(title.value,author.value,pages.value,read.value)  ||
-//              new Drama(title.value,author.value,pages.value,read.value) ||
-//              new Age(title.value,author.value,pages.value,read.value)  ||
-//              new Fantasy(title.value,author.value,pages.value,read.value)||
-//              new Fiction(title.value,author.value,pages.value,read.value);
-            
+   const book = new Book(title.value,author.value,pages.value,read.value)
 
-//              myLibrary = myLibrary.push(book);
+        return myLibrary.push(book);
 
-// return book;
-// }addBookToLibrary();
-
-
-function displayBook(){
+ } 
+ 
+ 
+ function displayBook(){
 
   myLibrary.forEach((book)=>{
     console.log(book);
-  }) ;
+    book= document. createElement("div");
+    book.setAttribute("class", "book");
+   const deleteBtn = document .createElement("button");
+   const readBtn = document. createElement("button");
+    book. appendChild(deleteBtn);
+    book. appendChild(readBtn);
+    deleteBtn.classList.add("delete");
+    readBtn.classList.add("read");
+    deleteBtn.textContent="Delete";
+    readBtn.textContent = "read";
+    mainPage.textContent = book;
+    book.textContent= Book.info()
+  })
 }displayBook();
+
+
+
+// DOM manipulation
+
+const add = document. querySelector(".add img");
+const dialog= document. querySelector("dialog");
+const closeBtn = document. querySelector(".close");
+const input = document.querySelectorAll("input");
+let select = document. querySelector("select");
+let genreInput = document.querySelector("#genre-input");
+const addBook = document.querySelector("#add-book");
+const mainPage = document.querySelector("main")
+
+add.addEventListener("click",() => {
+    dialog.showModal()
+});
+
+closeBtn.addEventListener("click",(e)=>{
+   dialog.close();
+   input.reset();   
+});
+
+select.addEventListener("change",()=>{
+   genreInput.value = select.value;
+
+});
+
+addBook.addEventListener("click",()=>{
+ addBookToLibrary();
+})
+
